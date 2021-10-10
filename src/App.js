c7732053cd921132104d6d9064449e96032e5a24
Kitty <zainab_97@hotmail.co.uk>
2021-10-10 17:57:33 +0300

    All but task 4 done

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import Home from "./components/Home";
 // Components
 import ProductList from "./components/ProductList";
 //styling
-import "./App.css"
-import Detail from './components/Detail'
-
+import "./App.css";
+import Detail from "./components/Detail";
+import { Route, Switch } from "react-router";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
-    <div >
-      <Home />
-      <ProductList />
-      <Detail />
+    <div>
+      <Navbar />
+      <Switch>
+        <Route exact path="/Productlist">
+          <ProductList />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Detail />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <header>
+      <ul>
+        <li>
+          <Link to="/" className="active">
+            <img
+              className="imageEdit"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBlJmMJNSX57Oc7mfB0hXdcHcVFIxz5RH76Dl0A8Yd5RRzEeI0eBHKZ3KdkMW40FLIoXQ&usqp=CAU"
+              alt=""
+            />
+          </Link>
+        </li>
+        <li>
+          <Link to="/Productlist">ProductList</Link>
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default Navbar;
